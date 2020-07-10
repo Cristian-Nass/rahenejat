d6808f29bc003b1948be16cbf5a18028bd8085ee
Cristian Nass <kristian.nasiri@gmail.com>
2020-07-11 01:43:55 +0200

    Add mehdi and compeleted some components

--- src/app/members/members.component.ts
+++ src/app/members/members.component.ts
@@ -8,8 +8,23 @@ import { Component, OnInit } from '@angular/core';
 export class MembersComponent implements OnInit {
 
   constructor() { }
-  members : any[] = [
-    {id: 1, name: 'کریستین', text: 'در حال آماده سازی', img: 'assets/members/cristian.png'},
+  members: any[] = [
+    {
+      id: 1,
+      name: 'کریستین',
+      text1: 'تعمید : ۲۷ می ۲۰۱۸',
+      text2: 'واقعا خوشحالم که عیسی مسیح رو به عنوان ناجی و خدای خودم پذیرفتم ،',
+      text3: 'مسیحیت دین نیست ، مسیحیت راه نجات و رستگاری است .',
+      img: 'assets/members/cristian.png'
+    },
+    {
+      id: 2,
+      name: 'مهدی خدایی',
+      text1: 'تعمید : ۸ جولای ۲۰۱۸',
+      text2: 'تنها راه شناخت خداوند و رسیدن به رستگاری کامل راه مسیحیت است . ',
+      text3: 'خداوندی که گناهان ما را به خود گرفت و جانش را برای رستگاری ما فدا کرد .',
+      img: 'assets/members/mehdilogo.png'
+    }
 ];
   ngOnInit() {
   }
